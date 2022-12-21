87553f26423234ce80cc1233cdf9a813dd9bdac6
DastinDV <komander_72@rambler.ru>
2022-12-21 14:40:32 +0300

    Some test changes

--- web.py
+++ web.py
@@ -10,7 +10,7 @@ todos = functions.get_todos()
 
 st.title("My todo App")
 st.subheader("This is my todo app")
-st.write("For increasing your productivity")
+st.write("For increasing your productivity!")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
